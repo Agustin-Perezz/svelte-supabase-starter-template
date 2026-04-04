3f79a14877156c8cc3d96769b58d0cbd576fac17
Agustin-Perezz <agustinnicolas806@gmail.com>
2026-04-04 10:42:27 -0300

    Remove legacy domain, schemas, and server directories

--- src/lib/domain/models/book.ts
+++
@@ -1,5 +0,0 @@
-import type { Database } from '$domain/types/database.types';
-
-export type Book = Database['public']['Tables']['books']['Row'];
-export type BookInsert = Database['public']['Tables']['books']['Insert'];
-export type BookUpdate = Database['public']['Tables']['books']['Update'];
