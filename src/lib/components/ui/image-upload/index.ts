d43740a8bdc90e62c7e4dd6ee1bd73f0191791fa
Agustin-Perezz <agustinnicolas806@gmail.com>
2026-04-29 18:24:48 -0300

    Add image-upload UI component with preview and progress

---
+++ src/lib/components/ui/image-upload/index.ts
@@ -0,0 +1,10 @@
+import Root, { type ImageUploadProps } from './image-upload.svelte';
+import type { ImageUploadItem } from './image-upload.svelte.js';
+
+export {
+  Root,
+  //
+  Root as ImageUpload,
+  type ImageUploadProps,
+  type ImageUploadItem
+};
